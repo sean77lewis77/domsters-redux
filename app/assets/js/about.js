460b9618cdf48f21adec6aa61e9d9d09ee7fc409
Philip Howley <howley.phil@gmail.com>
2017-10-20 16:31:29 -0700

    add domsters about javascript

--- app/assets/js/about.js
+++ app/assets/js/about.js
@@ -1,41 +1,43 @@
 function showSection(id) {
-    console.log(`I called this function from another function and the id is ${id}`);
-    const divs = document.getElementsByTagName('div');
-    for (var i = 0; i < divs.length; i++) {
-        if (divs[i].className.indexOf('section') === -1) continue;
-        if (divs[i].getAttribute('id') != id) {
-            divs[i].style.display = 'none';
+    console.log(`I called this function (showSection) from another function (prepareInternalNav) and the id (argument passed to showSection) is ${id}`);
+    //     const divs = document.getElementsByTagName('div');
+    //     for (var i = 0; i < divs.length; i++) {
+    //         if (divs[i].className.indexOf('section') === -1) continue;
+    //         if (divs[i].getAttribute('id') != id) {
+    //             divs[i].style.display = 'none';
 
-        } else {
-            divs[i].style.display = 'block';
-        }
-    }
+    //         } else {
+    //             divs[i].style.display = 'block';
+    //         }
+    //     }
 }
 
 function prepareInternalNav() {
-    // check old browsers
+    //     // check old browsers
     if (!document.getElementsByTagName) return false;
     if (!document.getElementById) return false;
-    // make sure the element is on the page
+
+    //     // make sure the element is on the page
     if (!document.getElementById('internalNav')) return false;
-    // grab the container of all the links
+    //     // grab the container of all the links
     const nav = document.getElementById('internalNav');
     // console.log(nav);
-    // grab all the links
+    //     // grab all the links
     const links = nav.getElementsByTagName('a');
-    // console.log(links.length);
-    // loop through the links
+    // console.log(links);
+    //     // loop through the links
     for (let i = 0; i < links.length; i++) {
-        // grab just the name
+        // console.log('I won\'t be bad');
+        //         // grab just the name
         const sectionId = links[i].getAttribute('href').split('#')[1];
         // console.log(sectionId);
-        // check to see if the name exists
+        //         // check to see if the name exists
         if (!document.getElementById(sectionId)) continue;
-        // hide all content
+        //         // hide all content
         document.getElementById(sectionId).style.display = 'none';
-        // trick to keep 'this'
+        //         // trick to keep 'this'
         links[i].destination = sectionId;
-        // add clicks to all links and call another function and pass the name (id)
+        //         // add clicks to all links and call another function and pass the name (id)
         links[i].onclick = function() {
             showSection(this.destination);
             return false;
@@ -44,5 +46,8 @@ function prepareInternalNav() {
     }
 
 }
+
+
+
 
 addLoadEvent(prepareInternalNav);
